f23a7d482aaff31109a2775f508c6b637fd20cc3
Ichunjo <ichunjo.le.terrible@gmail.com>
2026-04-28 18:13:36 +0200

    enhance command line interface styling

--- src/commands/cli.rs
+++ src/commands/cli.rs
@@ -1,8 +1,23 @@
 use anyhow::Result;
+use clap::builder::styling::Styles;
 use clap::{Parser, Subcommand};
 
+pub const CLAP_STYLING: Styles = Styles::styled()
+    .header(clap_cargo::style::HEADER)
+    .usage(clap_cargo::style::USAGE)
+    .literal(clap_cargo::style::LITERAL)
+    .placeholder(clap_cargo::style::PLACEHOLDER)
+    .error(clap_cargo::style::ERROR)
+    .valid(clap_cargo::style::VALID)
+    .invalid(clap_cargo::style::INVALID);
+
 #[derive(Parser, Debug)]
-#[command(bin_name = env!("PYAPP_PROJECT_NAME"), version, disable_help_subcommand = true)]
+#[command(
+    bin_name = env!("PYAPP_PROJECT_NAME"),
+    version,
+    disable_help_subcommand = true,
+    styles = CLAP_STYLING
+)]
 pub struct Cli {
     #[command(subcommand)]
     command: Commands,
